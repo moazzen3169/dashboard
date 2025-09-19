ea793d731a7259d92d44ea699abc5cdcb9c401ca
hadi3169 <moazzenhadi46@gmail.com>
2025-09-20 01:05:58 +0330

    add support

---
+++ support.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    body{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100vh;
+        background-color: #121116;
+        color: aliceblue;
+        overflow: hidden;
+
+    }
+
+    .title{
+        direction: rtl;
+        font-size: 30px;
+        font-family: peyda;
+        max-width: 200px;
+        text-align: justify;
+    }
+
+    .back_btn{
+        width: 200px;
+        margin-top: 10px;
+        height: 30px;
+        font-family: peyda;
+        border: none;
+        background-color: #70ff74;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: #121116;
+        text-decoration: none;
+    }
+</style>
+
+
+<body>
+
+    <div class="title">
+        کــاربر مورد نظـــر یافـــــــت نشـــــــــد لطفابعدا با هـادی تماس بگیـــــرید!!
+    </div>
+    <a  href="dashboard.php" class="back_btn">بازگشت به داشبورد</a>
+    
+</body>
+</html>
+
