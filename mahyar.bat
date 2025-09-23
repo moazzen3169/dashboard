e94e100322fb7684ae34688556e22c17da96c815
hadi3169 <moazzenhadi46@gmail.com>
2025-09-23 16:43:42 +0330

    add all files as mahyar 1404/07/1

--- mahyar.bat
+++ mahyar.bat
@@ -8,15 +8,32 @@ REM بررسی روشن بودن WampServer
 tasklist /FI "IMAGENAME eq wampmanager.exe" | find /I "wampmanager.exe" >nul
 
 if "%ERRORLEVEL%"=="0" (
-    echo WampServer در حال اجراست...
+echo WampServer در حال اجراست...
 ) else (
-    echo WampServer روشن نیست. در حال اجرا...
-    start "" %WAMP_PATH%
-    echo صبر برای اجرای WampServer...
-    timeout /t 10 /nobreak >nul
+echo WampServer روشن نیست. در حال اجرا...
+start "" %WAMP_PATH%
+echo در حال انتظار برای اجرای کامل WampServer...
+
+REM انتظار تا زمانی که WampServer کاملاً اجرا شود
+:CHECK_WAMP
+timeout /t 3 /nobreak >nul
+tasklist /FI "IMAGENAME eq wampmanager.exe" | find /I "wampmanager.exe" >nul
+if "%ERRORLEVEL%"=="1" (
+    echo هنوز در حال اجرای WampServer...
+    goto CHECK_WAMP
+)
+
+echo WampServer با موفقیت اجرا شد.
+
+REM انتظار اضافی برای اطمینان از کامل شدن اجرای سرویس‌ها
+echo در حال انتظار برای راه‌اندازی سرویس‌ها...
+timeout /t 15 /nobreak >nul
 )
 
 REM باز کردن لینک در Google Chrome
-start chrome "http://localhost/pro-modir/dashboard.php"
+echo در حال باز کردن مرورگر...
+start chrome "http://localhost/dashboard/factor-products.php"
+
+echo عملیات با موفقیت انجام شد.
 
 endlocal
